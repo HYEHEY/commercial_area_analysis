65f58e0a4f8cab5dc069c8f74429439e9015957b
P1rain <dsk7041@gamail.com>
2023-08-09 15:46:51 +0900

    최산화

--- crawling_test_file/xycrawling.py
+++ crawling_test_file/xycrawling.py
@@ -160,140 +160,37 @@ rest_api_key = "52756ac9cbee83797b5dd4e41ff78344"  # 여기서 개인적으로 �
 
 kakao = KakaoLocalAPI(rest_api_key)
 
-df = pd.read_csv('price_output.csv', encoding='utf-8')
+df = pd.read_csv('./boo_data/Jeju.csv', encoding='utf-8')
 
-INF_ID = list()
-INF_TOURIST = list()
-INF_ADDRESS = list()
-INF_MT1_list = list()
-INF_CS2_list = list()
-INF_PS3_list = list()
-INF_SC4_list = list()
-INF_AC5_list = list()
-INF_PK6_list = list()
-INF_OL7_list = list()
-INF_SW8_list = list()
-INF_BK9_list = list()
-INF_CT1_list = list()
-INF_AG2_list = list()
-INF_PO3_list = list()
-INF_AT4_list = list()
-INF_AD5_list = list()
-INF_FD6_list = list()
-INF_CE7_list = list()
-INF_HP8_list = list()
-INF_PM9_list = list()
+INF_id_list = list()
+INF_TOURIST_list = list()
+INF_ADDRESS_list = list()
+INF_X_list = list()
+INF_Y_list = list()
 
 for i in range(len(df)):
-    id_ = df.loc[i, 'INF_ID']
-    tourist = df.loc[i, 'INF_TOURIST']
-    addr = df.loc[i, 'INF_ADDRESS']
-    x_coordinate = df.loc[i, 'INF_X']
-    y_coordinate = df.loc[i, 'INF_Y']
-    result_01 = kakao.search_keyword(query=addr, x=x_coordinate, y=y_coordinate, radius=330)
-    INF_MT1 = 0
-    INF_CS2 = 0
-    INF_PS3 = 0
-    INF_SC4 = 0
-    INF_AC5 = 0
-    INF_PK6 = 0
-    INF_OL7 = 0
-    INF_SW8 = 0
-    INF_BK9 = 0
-    INF_CT1 = 0
-    INF_AG2 = 0
-    INF_PO3 = 0
-    INF_AT4 = 0
-    INF_AD5 = 0
-    INF_FD6 = 0
-    INF_CE7 = 0
-    INF_HP8 = 0
-    INF_PM9 = 0
+    # type_ = df.loc[i, '']
+    id_num = i+1571
+    tourist = df.loc[i, '관광지명']
+    addr = df.loc[i, '주소']
+    result_01 = kakao.search_address(addr)
     print(result_01)
     test = result_01['documents']
-    for j in range(len(test)):
-        category_group_code = test[j]['category_group_code']
-        if category_group_code == 'MT1':
-            INF_MT1 += 1
-        elif category_group_code == 'CS2':
-            INF_CS2 += 1
-        elif category_group_code == 'PS3':
-            INF_PS3 += 1
-        elif category_group_code == 'SC4':
-            INF_SC4 += 1
-        elif category_group_code == 'AC5':
-            INF_AC5 += 1
-        elif category_group_code == 'PK6':
-            INF_PK6 += 1
-        elif category_group_code == 'OL7':
-            INF_OL7 += 1
-        elif category_group_code == 'SW8':
-            INF_SW8 += 1
-        elif category_group_code == 'BK9':
-            INF_BK9 += 1
-        elif category_group_code == 'CT1':
-            INF_CT1 += 1
-        elif category_group_code == 'AG2':
-            INF_AG2 += 1
-        elif category_group_code == 'PO3':
-            INF_PO3 += 1
-        elif category_group_code == 'AT4':
-            INF_AT4 += 1
-        elif category_group_code == 'AD5':
-            INF_AD5 += 1
-        elif category_group_code == 'FD6':
-            INF_FD6 += 1
-        elif category_group_code == 'CE7 ':
-            INF_CE7 += 1
-        elif category_group_code == 'HP8':
-            INF_HP8 += 1
-        elif category_group_code == 'PM9':
-            INF_PM9 += 1
-    INF_ID.append(id_)
-    INF_TOURIST.append(tourist)
-    INF_ADDRESS.append(addr)
-    INF_MT1_list.append(INF_MT1)
-    INF_CS2_list.append(INF_CS2)
-    INF_PS3_list.append(INF_PS3)
-    INF_SC4_list.append(INF_SC4)
-    INF_AC5_list.append(INF_AC5)
-    INF_PK6_list.append(INF_PK6)
-    INF_OL7_list.append(INF_OL7)
-    INF_SW8_list.append(INF_SW8)
-    INF_BK9_list.append(INF_BK9)
-    INF_CT1_list.append(INF_CT1)
-    INF_AG2_list.append(INF_AG2)
-    INF_PO3_list.append(INF_PO3)
-    INF_AT4_list.append(INF_AT4)
-    INF_AD5_list.append(INF_AD5)
-    INF_FD6_list.append(INF_FD6)
-    INF_CE7_list.append(INF_CE7)
-    INF_HP8_list.append(INF_HP8)
-    INF_PM9_list.append(INF_PM9)
+    x_coordinate = test[0]['x']
+    y_coordinate = test[0]['y']
+    INF_id_list.append(id_num)
+    INF_TOURIST_list.append(tourist)
+    INF_ADDRESS_list.append(addr)
+    INF_X_list.append(x_coordinate)
+    INF_Y_list.append(y_coordinate)
 
 df_new = pd.DataFrame(
     {
-        "INF_ID": INF_ID,
-        "INF_TOURIST": INF_TOURIST,
-        "INF_ADDRESS": INF_ADDRESS,
-        "INF_MT1": INF_MT1_list,
-        "INF_CS2": INF_CS2_list,
-        "INF_PS3": INF_PS3_list,
-        "INF_SC4": INF_SC4_list,
-        "INF_AC5": INF_AC5_list,
-        "INF_PK6": INF_PK6_list,
-        "INF_OL7": INF_OL7_list,
-        "INF_SW8": INF_SW8_list,
-        "INF_BK9": INF_BK9_list,
-        "INF_CT1": INF_CT1_list,
-        "INF_AG2": INF_AG2_list,
-        "INF_PO3": INF_PO3_list,
-        "INF_AT4": INF_AT4_list,
-        "INF_AD5": INF_AD5_list,
-        "INF_FD6": INF_FD6_list,
-        "INF_CE7": INF_CE7_list,
-        "INF_HP8": INF_HP8_list,
-        "INF_PM9": INF_PM9_list
+        "INF_ID": INF_id_list,
+        "INF_TOURIST": INF_TOURIST_list,
+        "INF_ADDRESS": INF_ADDRESS_list,
+        "INF_X": INF_X_list,
+        "INF_Y": INF_Y_list
     }
 )
-df_new.to_csv('Jeju_TB_INFRASTRUCTURE.csv', index=False)
+df_new.to_csv('price_output.csv', index=False)
