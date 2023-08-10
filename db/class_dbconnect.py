eabff18a2c56cc28cb82808852d4fcaa9e240235
hyebin <althms107@gmail.com>
2023-08-10 19:35:53 +0900

    년도별 방문 통계 및 주변 편의점 매출 현황 완료

--- db/class_dbconnect.py
+++ db/class_dbconnect.py
@@ -57,17 +57,10 @@ class DBConnector:
 
     def find_realty_info(self, nm_):
         """부동산 정보 반환"""
-        c = self.start_conn()
-        c.execute(f"select * from \"TB_REALTY\" where \"REA_TOURIST\" = '{nm_}'")
-        info_list = c.fetchall()
-        # 부동산 ID, 관광지명, 부동산 주소, 상가 종류, 경도, 위도, 계약 면적, 전역 면적, 부동산 분류, 임대료, 보증금, 매매가
-
-        if len(info_list) == 0:
-            return None
+        info_list = self.read_db(1, "TB_REALTY", "REA_TOURIST", f"{nm_}")
 
         find_result_list = list()
         for info in info_list:
-            # print(f"{info=}")
             realty_obj = Realty(*info)
             find_result_list.append(realty_obj)
 
@@ -76,15 +69,8 @@ class DBConnector:
 
     def find_tourist_info(self, nm_):
         """관광지 정보 반환"""
-        c = self.start_conn()
-        c.execute(f"select * from \"TB_TOURIST_INFO\" where \"TOU_NAME\" = '{nm_}')")
-        # 관광지 ID, 지역명, 관광지명, 주소, 분류, 경도, 위도, 관광객수
-
-        info_list = c.fetchone()
-        print(f"관광지명 : {nm_}")
-
-        if len(info_list) == 0:
-            return None
+        info_list = self.read_db(2, "TB_TOURIST_INFO", "TOU_NAME", f"{nm_}")
+        print(f"관광지_정보: {info_list}")
 
         find_result_list = list()
         tourist_obj = TouristInfo(*info_list)
@@ -93,20 +79,71 @@ class DBConnector:
         self.end_conn()
         return find_result_list
 
-    def find_year_peple(self, addr_):
-        """년도별 방문 통계 반환"""
-        # todo: 관광지명 기준으로 년도별 방문 통계 리턴
-        pass
-
-    def find_store_average(self):
+    def find_store_average(self, nm_):
         """관광지 주변 편의점 평균 정보 반환"""
         # todo: 관광지명 기준으로 편의점 평균 매출 리턴
-        pass
+        info_list = self.read_db(2, "TB_BUSINESS_AVERAGE", "BUS_TOURIST", f"{nm_}")
+        print(f"평균_정보: {info_list}")
 
-    def find_infra_info(self):
+        find_result_list = list()
+        tourist_obj = BusinessAverage(*info_list)
+        find_result_list.append(tourist_obj)
+
+        self.end_conn()
+        return find_result_list
+
+    def find_infra_info(self, addr_):
         """매물 주변 상권 정보 반환"""
         # todo: 매물 주소를 기준으로 주변 인프라 정보 리턴
-        pass
+        info_list = self.read_db(2, "TB_INFRASTRUCTURE", "INF_ADDRESS", f"{addr_}")
+        print(f"주변_상권: {info_list}")
+
+        find_result_list = list()
+        tourist_obj = Infrastructure(*info_list)
+        find_result_list.append(tourist_obj)
+
+        self.end_conn()
+        return find_result_list
+
+    def find_year_peple(self, nm_):
+        """년도별 방문 통계 반환"""
+        info_list = self.read_db(1, "TB_YEAR_TOURIST", "YEA_TOURIST", f"{nm_}")
+        print(f"년도별_방문_통계: {info_list}")
+
+        find_result_list = list()
+        for info in info_list:
+            realty_obj = YearTourist(*info)
+            find_result_list.append(realty_obj)
+
+        self.end_conn()
+        return find_result_list
+
+    def read_db(self, num_, table_, column_, condition_):
+        """테이블 조회"""
+        c = self.start_conn()
+        # c.execute(f"select * from \"{table_}\" where \"{column_}\" = '{condition_}'")
+        c.execute(f"select * from \"{table_}\" where \"{column_}\" = (%s)", (condition_, ))
+        # print(f"조건 : {condition_}")
+
+        if num_ == 1:
+            info_list = c.fetchall()
+        elif num_ == 2:
+            info_list = c.fetchone()
+
+        if len(info_list) == 0:
+            return None
+
+        return info_list
+
+    def search_addr(self, realty_obj: Realty):
+        """주소 조회 후 관광지명 반환"""
+        addr_ = realty_obj.rea_address
+        # print(f"{addr_=}")
+        tourist_name = self.read_db(1, "TB_REALTY", "REA_ADDRESS", f"{addr_}")
+        print(tourist_name[0][1])
+
+        self.find_year_peple(tourist_name[0][1])
+        self.find_store_average(tourist_name[0][1])
 
 
 if __name__ == '__main__':
@@ -114,6 +151,5 @@ if __name__ == '__main__':
     db.test_option = True
 
     ### test ###
-    a = db.find_realty_info('안목해변')
-    # a = db.find_tourist_info('수성못')
-    print(a)
+    info_ = db.find_realty_info('에버랜드')
+    db.search_addr(info_[0])
